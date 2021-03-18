ea021164adb1ba8591165461d911c4bbcf196ddb
wangsitbintang <wangsitbintangdarmawan@gmail.com>
2021-03-18 21:40:51 +0700

    classtaxi

---
+++ Taxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine (" Drive Name      : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine(" On Duty     : Yes", OnDuty); 
+            }
+            else
+            {
+                Console.WriteLine(" On Duty     : No", OnDuty);
+            }
+
+            Console.WriteLine(" Number Of Passenger : {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai menjemput penumpang", DriverName);
+        }
+        
+    }
+
+}
